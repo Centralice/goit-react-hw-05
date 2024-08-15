b77b71af7281fef6133db36d1376137341a51ebf
Centralice <108938330+Centralice@users.noreply.github.com>
2024-08-15 15:32:46 +0200

    query added to address

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import MovieList from "../../components/MovieList/MovieList";
 import s from "./MoviesPage.module.css";
 import { fetchByQuery } from "../../services/api";
+import { useSearchParams } from "react-router-dom";
 
 const MoviesPage = () => {
   const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,8 +19,10 @@ const MoviesPage = () => {
     }
     e.target.reset();
     setMovies(response.data.results);
+    searchParams.set("query", query);
+    setSearchParams(searchParams);
   };
-  
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
